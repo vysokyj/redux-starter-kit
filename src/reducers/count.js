5e050390902d67afe14dd5cfb7f166ca54152141
Jiří Vysoký <jiri.vysoky@ys.cz>
2016-04-25 16:18:50 +0200

    Updated store

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,17 +1,25 @@
 import Immutable from "immutable";
 import { INCREASE, DECREASE } from '../constants/ActionTypes'
 
-const initialState =  Immutable.fromJS({
+const initialState = Immutable.fromJS({
     number: 1
 });
+
+function increase(state) {
+  return state.set("number", state.get("number") + action.amount);
+}
+
+function decrease(state) {
+  return state.set("number", state.get("number") - action.amount);
+}
 
 export default function update(state = initialState, action) {
     return state
     switch (action.typy) {
         case INCREASE:
-            return state.set("number", state.number + action.amount);
+            return increase(state);
         case DECREASE:
-            return state.set("number", state.number - action.amount);
+            return decrease(state);
         default:
             return state;
     }
