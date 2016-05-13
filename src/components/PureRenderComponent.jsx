2c4841cd3d8b015d64cf7da2d66e6863e6317df1
Jiří Vysoký <jiri.vysoky@gmail.com>
2016-05-14 00:22:23 +0200

    Added bootswatch theme

--- src/components/PureRenderComponent.jsx
+++ src/components/PureRenderComponent.jsx
@@ -4,12 +4,12 @@ var shallowCompare = require("react-addons-shallow-compare");
 
 class PureRenderComponent extends React.Component {
 
-    constructor() {
-        super();
-        this.props = null;
-        this.state = null;
-        this.refs = null;
-    }
+    // constructor() {
+    //     super();
+    //     this.props = null;
+    //     this.state = null;
+    //     this.refs = null;
+    // }
 
     shouldComponentUpdate(nextProps, nextState) {
         return shallowCompare(this, nextProps, nextState);
