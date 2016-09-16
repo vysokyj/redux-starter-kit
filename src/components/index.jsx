836fea38446b3774972e56fdbfad549c48e0d493
Jiří Vysoký <jiri.vysoky@gmail.com>
2016-09-16 19:57:11 +0200

    Added React Intl

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {render} from "react-dom";
 import {Provider} from "react-redux";
+import {IntlProvider} from "react-intl-redux";
 import {Router, Route, IndexRoute} from "react-router"
 import App from "./App";
 import CountPage from "./CountPage";
@@ -9,12 +10,14 @@ import TodoPage from "./TodoPage";
 export function createRouter(store, history) {
     render(
         <Provider store={store}>
-            <Router history={history}>
-                <Route path="/" component={App}>
-                    <IndexRoute component={CountPage}/>
-                    <Route path="todos" component={TodoPage}/>
-                </Route>
-            </Router>
+            <IntlProvider>
+                <Router history={history}>
+                    <Route path="/" component={App}>
+                        <IndexRoute component={CountPage}/>
+                        <Route path="todos" component={TodoPage}/>
+                    </Route>
+                </Router>
+            </IntlProvider>
         </Provider>,
         document.getElementById("root")
     );
